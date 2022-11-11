d45e247b964d0523d7fc16b8f0ca1ec55a40f4ad
LeandroGiacomini <gel.giacomini@gmail.com>
2022-11-11 11:32:30 -0300

    cpf

--- 11-11/validacao.py
+++ 11-11/validacao.py
@@ -1,6 +1,7 @@
 #faça um algoritmo que leia o nome completo de uma pesso a e coloque dentro de uma lista
 #o algoritmo deve usar um metodo para colocar o nome na lista
 #validacao: caracteres especiais no nome, numeros ou vazio
+#cpf do bruno: 46521427814
 
 def metodo():
     
