66524fafb00518f4b867e0c17006b9424c6d9093
LeandroGiacomini <gel.giacomini@gmail.com>
2022-11-11 10:53:24 -0300

    1

--- 04-11/ana1.py
+++ 04-11/ana1.py
@@ -1,15 +1,11 @@
+nome = input("Digite seu nome completo: ")
+idade = int(input("Digite a sua idade: "))
+cpf = int(input("Digite seu CPF: "))
 
-def percorrer():
-    nome = input("Digite seu nome: ")
-    idade = int(input("Digite a sua idade: "))
-    cpf = int(input("Digite seu CPF: "))
+def nome():
 
     lista  = [nome, idade, cpf]
     print(lista)
 
-    if not idade.replace('.', '').replace(',', '').isnumeric():
-        print("Número Inválido")
-    else: print("Ok")
-        
+nome()
 
-percorrer()
---
+++ 11-11/validacao.py
@@ -0,0 +1,9 @@
+#faça um algoritmo que leia o nome completo de uma pesso a e coloque dentro de uma lista
+#o algoritmo deve usar um metodo para colocar o nome na lista
+#validacao: caracteres especiais no nome, numeros ou vazio
+
+def metodo():
+    
+    lista = [input("Digite seu nome completo: ")]
+
+metodo()
